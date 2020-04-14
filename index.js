71076e24edfcab1d857e799906776c2c4c3ba33f
TechnicalUnsupport <63417398+TechnicalUnsupport@users.noreply.github.com>
2020-04-14 18:25:37 +0200

    new features!
    added:
    -msg command to make the bot send a message
    -github cmd to link here
    -info to provide channel links
    -general aesthetic improved

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = 'tau ';
 var embedcolor = 0x00FFFF;
 
-const token = 'get rekt noob';
+const token = 'TOP SECRET';
 
 bot.on('ready', () =>{
     console.log('bot online');
@@ -16,8 +16,8 @@ bot.on('message', msg=>{
     if(msg.content === "hello"){
         msg.channel.send('hewo uwu');
     }
- })
-
+ }
+)
 bot.on('message', msg=>{
 
     let args = msg.content.substring(prefix.length).split(" ");
@@ -26,23 +26,47 @@ bot.on('message', msg=>{
         case 'ign':
             const ign = new MessageEmbed()
             .setColor(embedcolor)
-            .addField('Minecraft:','n/a')
-            .addField('Steam','n/a')
+            .addField('Minecraft:','Just_Tau')
+            .addField('Steam','Tau')
             msg.channel.send(ign);
             break;
         case 'help':
             const help = new MessageEmbed()
+            .setTitle('Help')
+            .setDescription('list of commands')
             .setColor(embedcolor)
             .addField('tau ign',"gives tau's igns")
             .addField('tau gay',"you'll see :)")
             .addField('tau ping','pings tau')
+            .addField('tau msg','anyhing past msg will be sent by the bot(command message will be deleted)')
+            .addField('tau github',"will link to the bot's github page")
+            .addField('tau info', "links to tau's youtube channel and twitch")
             msg.channel.send(help);
             break;
         case 'gay':
-            msg.channel.send('VERY GAY :Afrigay:')
+            msg.channel.send('VERY GAY :rainbow_flag:')
             break;
         case 'ping':
             msg.reply('lol thought you would ping tau?')
+            break;
+        case 'msg':
+            msg.delete()
+            msg.channel.send(args.join(' ').slice(3))
+            break;
+        case 'github':
+            const gh = new MessageEmbed()
+            .setTitle('Github')
+            .setURL('https://github.com/TechnicalUnsupport/taubot')
+            .setColor(embedcolor)
+            msg.channel.send(gh)
+            break;
+        case 'info':
+            const I = new MessageEmbed()
+            .setTitle('Info')
+            .setColor(embedcolor)
+            .addField('Youtube','https://www.youtube.com/channel/UC8e7qlj7-9Up6Jz7zTblt7A/about')
+            .addField('Twitch','https://www.twitch.tv/justtau')
+            msg.channel.send(I)
             break;
     }
 })
@@ -53,5 +77,6 @@ bot.on('guildMemberAdd', member=>{
 
     channel.send(`Welcome to the server, ${member}, please read the rules`)
 });
+
 
 bot.login(token);
